dc9451d60ee6eb0037326c8a10bb217f8faa5340
lybb-web <lybb@uci.edu>
2024-08-14 18:50:14 -0700

    Wrote a book constructor in javascript

---
+++ library.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+         
+        return  this.read ? this.title + " by " + this.author + ", " + this.pages + " pages, already read" : 
+        this.title + " by " + this.author + ", " + this.pages + " pages, not read yet";
+    }
+}
+
+let book1 = new Book("Diary of a Wimpy Kid", "Jeff Kinney", 217, true);
+console.log(book1.info());
